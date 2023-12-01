6af807f1426a0ebc425db6f84e507c9b4e2cfa3a
Krishna Manoj PVR <119659348+krishnamanojpvr@users.noreply.github.com>
2023-12-01 08:42:01 +0530

    Create flask_api.py

---
+++ TOH(30-11-23)/TOH_server/flask_api.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import time
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+model = keras.models.load_model('best_model.h5')
+
+def preprocess_image(image_path):
+    img = image.load_img(image_path, target_size=(64, 64))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0 
+    return img
+
+
+@app.route('/classify', methods=['POST'])
+def classify_image():
+    try:
+        file = request.files['image']
+        if(file):
+            image_path = 'flask_temp_image.jpg'
+            file.save(image_path)
+            processed_image = preprocess_image(image_path)
+            predictions = model.predict(processed_image)
+            predicted_class = np.argmax(predictions)
+            class_labels = ["Cracked", "Normal"]
+
+            result = {
+                "class": class_labels[predicted_class],
+                "confidence": float(predictions[0][predicted_class])
+            }
+
+            return jsonify(result)
+        else:
+            print("Can't access File")
+            return jsonify({"error": "Can't access File"})
+    except Exception as e :
+        print("No File Found")
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
